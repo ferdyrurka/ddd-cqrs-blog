1afbd3f00ebc605ef55a28c30b30254373fb1289
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-11-13 22:53:16 +0100

    Added BlogController

---
+++ web/src/Controller/BlogController.php
@@ -0,0 +1,75 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Command\AddPostCommand;
+use App\Domain\Entity\Post;
+use App\Form\AddPostForm;
+use App\Query\PostQuery;
+use App\Service\CommandBus;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class BlogController
+ * @package App\Controller
+ */
+class BlogController extends Controller
+{
+    /**
+     * @param PostQuery $postQuery
+     * @return array
+     * @Route("/", methods={"GET"}, name="index.blog")
+     * @Template("blog/index.html.twig")
+     */
+    public function indexAction(PostQuery $postQuery): array
+    {
+        return [
+            'posts' => $postQuery->getAll()
+        ];
+    }
+
+    /**
+     * @return array
+     * @param Request $request
+     * @Route("/add-post", methods={"GET"}, name="addPost.blog")
+     * @Template("blog/add-post.html.twig")
+     */
+    public function addPostAction(Request $request): array
+    {
+        $form = $this->createForm(AddPostForm::class, new Post());
+        $form->handleRequest($request);
+
+        return [
+            'form' => $form->createView()
+        ];
+    }
+
+    /**
+     * @param Request $request
+     * @param CommandBus $commandBus
+     * @return Response
+     * @throws \App\Exception\HandlerNotFoundException
+     * @Route("/add-post", methods={"POST"})
+     */
+    public function savePostAction(Request $request, CommandBus $commandBus): Response
+    {
+        $form = $this->createForm(AddPostForm::class, new Post());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $command = new AddPostCommand($form->getData());
+            $commandBus->handle($command);
+
+            return $this->redirect('/');
+        }
+
+        return $this->forward(BlogController::class . ':indexAction', [
+           'request' => $request
+        ]);
+    }
+}
