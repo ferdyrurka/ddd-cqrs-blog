6da19a6380eaddb217934450f2a5ba11d4643fa2
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2018-11-13 19:59:19 +0100

    PostRepository

---
+++ web/src/Repository/PostRepository.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Domain\Entity\Post;
+use App\Query\PostQuery;
+use App\Query\ViewObject\PostViewObject;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Common\Persistence\ManagerRegistry;
+
+/**
+ * Class PostRepository
+ * @package App\Repository
+ */
+class PostRepository extends ServiceEntityRepository implements PostQuery
+{
+    /**
+     * PostRepository constructor.
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Post::class);
+    }
+
+    public function getAll(): array
+    {
+        $posts = $this->findAll();
+
+        if (empty($posts)) {
+            return [];
+        }
+
+        $postsView = [];
+
+        foreach ($posts as $post) {
+            $postsView[] = new PostViewObject(
+                $post->getCreatedAt(),
+                $post->getContent(),
+                $post->getTitle()
+            );
+        }
+
+        return $postsView;
+    }
+
+    /**
+     * @param Post $post
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function remove(Post $post): void
+    {
+        $em = $this->getEntityManager();
+        $em->remove($post);
+        $em->flush();
+    }
+
+    /**
+     * @param Post $post
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function save(Post $post): void
+    {
+        $em = $this->getEntityManager();
+        $em->persist($post);
+        $em->flush();
+    }
+}
